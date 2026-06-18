75b76daa329c7ae30df3b818ef0512c17abbd064
jim yu <jim.yu@optus.com.au>
2026-06-18 12:01:04 +1000

    trigger build

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const common = require("./webpack.common.js");
 const packageJson = require("../package.json");
